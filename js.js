5556f5ea1f1feb9150b597f48d66662aadf58753
anthony-xiao <juniorxiao01@gmail.com>
2018-04-23 13:08:04 +1200

    add in number input function

--- js.js
+++ js.js
@@ -1,5 +1,21 @@
-// var clearOutput = clearOut(output);
 
-let clearOutput = () => {
+//function to clear out output field
+function clearOut () {
     document.getElementById('output').innerHTML = '';
 }
+
+
+//input button numbers into output field. 
+let getNumber = document.getElementsByClassName('numBtn')
+
+let numClick = function(i) {
+    let newOutput = document.getElementById('output')
+    let currentValue = getNumber[i].innerHTML;
+    console.log (currentValue);
+    newOutput.innerHTML += currentValue;
+}
+
+//formula
+let total = 0;
+
+
