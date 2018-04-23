dc5a6125dde2d1945ae9f0490e85cf0cb91b5745
anthony-xiao <juniorxiao01@gmail.com>
2018-04-23 21:47:31 +1200

    add add function, need to resolve input to output div

--- js.js
+++ js.js
@@ -1,9 +1,16 @@
 
-//function to clear out output field
+//function to clear out output field and console
 function clearOut () {
-    document.getElementById('output').innerHTML = '';
+    output.innerHTML = '';
+    console.clear();
+    total = 0;
+    equation = [];
 }
 
+//function to clear out output field
+function clearScreen () {
+    output.innerHTML = '';
+}
 
 //input button numbers into output field. 
 let getNumber = document.getElementsByClassName('numBtn')
@@ -15,7 +22,41 @@ let numClick = function(i) {
     newOutput.innerHTML += currentValue;
 }
 
-//formula
-let total = 0;
+//formulas
 
+//addition
+function getSum(total, num) {
+    if (num == null) {
+        return total;
+    } else {
+    return total + num;
+    }
+}
+
+let saveLastNum = function () {
+    let screenNum = output.innerHTML;
+    let oldNum = Number(screenNum);
+    return oldNum;
+}
+
+let total = 0;
+let equation = []
+//addEventListener
+
+let addition = function () {
+    equation.push(saveLastNum());
+    total = equation.reduce(getSum);
+    clearScreen();
+    return total;
+    output.innerHTML = total;
+}
+
+/*    let sign = document.getElementsByClassName('formu').innerHTML
+    console.log (sign);
+    */
+
+for (var x = 0; x < ops.lenght; x ++)
+    if (document.getElementsByClassName('ops').innerHTML = '+') {
+        total = oldNum + newNum
+    }
 
